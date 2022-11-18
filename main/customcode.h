3587d39fa4d91ff7b86e040d7449b21046208ff4
Crem2y <31000852+Crem2y@users.noreply.github.com>
2022-11-19 00:32:46 +0900

    Added Gamepad
    Added gamepad code and fixed repeatSpeed is not reset well

--- main/customcode.h
+++ main/customcode.h
@@ -40,34 +40,34 @@
 #define G_LD 0x89 // game_left_stick_down
 #define G_LL 0x8A // game_left_stick_left
 #define G_LR 0x8B // game_left_stick_right
-#define G_RU 0x8D // game_right_stick_up
-#define G_RD 0x8E // game_right_stick_down
+#define G_RU 0x8C // game_right_stick_up
+#define G_RD 0x8D // game_right_stick_down
 #define G_RL 0x8E // game_right_stick_left
 #define G_RR 0x8F // game_right_stick_right
-#define G_DU 0x90 // game_dpad_up
-#define G_DD 0x91 // game_dpad_down
-#define G_DL 0x92 // game_dpad_left
-#define G_DR 0x93 // game_dpad_right
+#define G_DU 0x90 // game_dpad_up    (not working well)
+#define G_DD 0x91 // game_dpad_down  (not working well)
+#define G_DL 0x92 // game_dpad_left  (not working well)
+#define G_DR 0x93 // game_dpad_right (not working well)
 #define G_ZU 0x94 // game_left_Zaxis+
 #define G_ZD 0x95 // game_left_Zaxis-
 #define G_ZL 0x96 // game_right_Zaxis+
 #define G_ZR 0x97 // game_right_Zaxis-
-#define G_01 0x98 // game_button_01
-#define G_02 0x99 // game_button_02
-#define G_03 0x9A // game_button_03
-#define G_04 0x9B // game_button_04
-#define G_05 0x9C // game_button_05
-#define G_06 0x9D // game_button_06
-#define G_07 0x9E // game_button_07
-#define G_08 0x9F // game_button_08
-#define G_09 0xA0 // game_button_09
-#define G_10 0xA1 // game_button_10
-#define G_11 0xA2 // game_button_11
-#define G_12 0xA3 // game_button_12
-#define G_13 0xA4 // game_button_13
-#define G_14 0xA5 // game_button_14
-#define G_15 0xA6 // game_button_15
-#define G_16 0xA7 // game_button_16
+#define G_01 0x98 // game_button_01 (A)
+#define G_02 0x99 // game_button_02 (B)
+#define G_03 0x9A // game_button_03 (X)
+#define G_04 0x9B // game_button_04 (Y)
+#define G_05 0x9C // game_button_05 (LB)
+#define G_06 0x9D // game_button_06 (RB)
+#define G_07 0x9E // game_button_07 (LT)
+#define G_08 0x9F // game_button_08 (RT)
+#define G_09 0xA0 // game_button_09 (MENU)
+#define G_10 0xA1 // game_button_10 (SELECT)
+#define G_11 0xA2 // game_button_11 (LS)
+#define G_12 0xA3 // game_button_12 (RS)
+#define G_13 0xA4 // game_button_13 (DPAD_UP)
+#define G_14 0xA5 // game_button_14 (DPAD_DOWN)
+#define G_15 0xA6 // game_button_15 (DPAD_LEFT)
+#define G_16 0xA7 // game_button_16 (DPAD_RIGHT)
 #define G_17 0xA8 // game_button_17
 #define G_18 0xA9 // game_button_18
 #define G_19 0xAA // game_button_19
